4efcd051f7b6684d048fdbb935e9ff6a727dda93
Zhenya Mamenko <mamenko@gmail.com>
2024-12-03 19:23:08 +0000

    [MOD] Changed i18n behavior

--- src/helpers/localization.ts
+++ src/helpers/localization.ts
@@ -3,6 +3,9 @@ import { I18n } from 'i18n-js';
 import translations from '@/constants/translations';
 
 const i18n = new I18n(translations);
+i18n.missingTranslation.register('return', (_i18n, scope, options) => scope as string);
+i18n.missingBehavior = 'return';
+i18n.defaultLocale = 'en';
 i18n.enableFallback = true;
 i18n.locale = getLocales()[0].languageCode ?? 'en';
 
